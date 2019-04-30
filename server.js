f8a8658d6c9e2b118e24c8ddd43e4b630f1c346d
emd5 <elizabeth.mahoney.dao@gmail.com>
2019-04-30 12:37:05 -0700

    refactored location constructor

--- server.js
+++ server.js
@@ -3,33 +3,64 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.static('./'));
 
 app.get('/hello', (request, response) => {
   response.status(200).send('Hello');
 });
 
-// custom create get request
+// Request for location (lat & lng)
 app.get('/location', (request,response) => {
   try{
     let geoData = require('./data/geo.json');
-    const newData = new Location(geoData.results[0]);
-    response.send(newData);
+    const newLocation = new Location(geoData[0], geoData[0].formatted_address, geoData[0].geometry.location.lat, geoData[0].geometry.lng)
+    // const newData = locationHelper(geoData);
+    response.send(newLocation);
   } catch(error){
-    console.log('There was an error')
+    console.log('There was an error with location')
     response.status(500).send('Status: ', error);
   }
-  
 });
+
+// Request for weather (time & forecast)
+app.get('/weather', (request,response) => {
+  let darkSky = require('./data/darksky.json');
+  try{
+    const newWeatherData = new Weather(darkSky);
+    response.send(newWeatherData);
+
+  } catch(error){
+    console.log('There was an error with weather')
+    response.status(500).send('Status: ', error);
+  }
+})
+
+//helper function
+
+function weatherHelper()
+// function locationHelper(geoData){
+//   const location = new Location(geoData, geoData.formatted_address, geoData.geometry.lat, geoData.geometry.lng );
+//   return location;
+// }
+
+// Location Constructor
+function Location(query, format, latitude, longitude){
+  this.searchQuery = query;
+  this.formattedQuery = format;
+  this.latitude = latitude;
+  this.longitude = longitude;
+}
+
+// Weather Constructor
+function Weather(searchQuery){
+  this.forecast = searchQuery.minutely.summary;
+  this.time = searchQuery.currently.time;
+}
 
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
 
-//create a constructor
-function Location(searchQuery){
-  this.searchQuery = searchQuery.formatted_address;
-  this.latitude = searchQuery.geometry.location.lat;
-  this.longitude = searchQuery.geometry.location.lng;
-}
